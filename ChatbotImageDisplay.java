4247d1178859e2ae952768c53f8dc3bf52256259
Ujjawal-Kantt <uk1287@srmist.edu.in>
2023-10-02 16:26:50 +0530

    New updates

--- ChatbotImageDisplay.java
+++ ChatbotImageDisplay.java
@@ -13,6 +13,10 @@ public class ChatbotImageDisplay extends JFrame {
   private JScrollPane imageScrollPane;
   private JPanel imagePanel;
   private JTextField inputField;
+  private JLabel nameLabel; // Label to display the user's name
+  private String userName = ""; // Store the user's name
+  private final int IMAGE_WIDTH = 100; // Desired width for scaled images
+  private final int IMAGE_HEIGHT = 100; // Desired height for scaled images
 
   public ChatbotImageDisplay() {
     // Initialize the JFrame
@@ -22,8 +26,8 @@ public class ChatbotImageDisplay extends JFrame {
     setLocationRelativeTo(null);
 
     // Create a map of words to image paths and populate it from a dataset file
-    wordToImagePath = readDatasetFile("C:\\Users\\samak\\Downloads\\SignLang\\images.txt"); // Replace with the path to
-    // your dataset file
+    wordToImagePath = readDatasetFile("C:\\D drive\\Projects\\SignLang\\images.txt"); // Replace with the path to your
+                                                                                      // dataset file
 
     // Create a JPanel to display the images
     imagePanel = new JPanel(new FlowLayout());
@@ -32,18 +36,41 @@ public class ChatbotImageDisplay extends JFrame {
     // This will allow us to scroll through the images if there are many of them
     imageScrollPane = new JScrollPane(imagePanel);
 
-    // Add the JScrollPane to the JFrame
-    add(imageScrollPane, BorderLayout.CENTER);
-
-    // Create a JTextField for entering sentences
+    // Create a JTextField for entering sentences (left-middle)
     inputField = new JTextField(40);
-    add(inputField, BorderLayout.NORTH);
+
+    // Create a JPanel for the input field and display button
+    JPanel inputPanel = new JPanel(new BorderLayout());
+    inputPanel.add(inputField, BorderLayout.CENTER);
 
     // Create a button to trigger image display
     JButton displayButton = new JButton("Display Images");
-    add(displayButton, BorderLayout.SOUTH);
+    inputPanel.add(displayButton, BorderLayout.EAST);
 
-    // Add action listener to the button
+    // Add the JScrollPane to the JFrame
+    add(imageScrollPane, BorderLayout.CENTER);
+
+    // Add the input panel to the left-middle
+    add(inputPanel, BorderLayout.WEST);
+
+    // Create a button to reset the displayed images and text field
+    JButton resetButton = new JButton("Reset");
+    add(resetButton, BorderLayout.EAST);
+
+    // Ask for the user's name and age
+    String name = JOptionPane.showInputDialog("Please enter your name:");
+    String age = JOptionPane.showInputDialog("Please enter your age:");
+
+    // Store the user's name
+    userName = name != null ? name : "";
+
+    // Create a label to display the user's name
+    nameLabel = new JLabel("Hello, " + userName);
+    nameLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+    nameLabel.setFont(new Font("Arial", Font.BOLD, 14));
+    add(nameLabel, BorderLayout.NORTH);
+
+    // Add an ActionListener to the displayButton
     displayButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         String sentence = inputField.getText().toLowerCase();
@@ -59,6 +86,21 @@ public class ChatbotImageDisplay extends JFrame {
 
         // Display the images
         displayImages(imagePaths);
+      }
+    });
+
+    // Add action listener to the reset button
+    resetButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        // Clear the JPanel
+        imagePanel.removeAll();
+
+        // Revalidate and repaint the JPanel
+        imagePanel.revalidate();
+        imagePanel.repaint();
+
+        // Clear the text field
+        inputField.setText("");
       }
     });
   }
@@ -85,18 +127,30 @@ public class ChatbotImageDisplay extends JFrame {
     // Clear the JPanel
     imagePanel.removeAll();
 
-    // Load and display each image
     for (String imagePath : imagePaths) {
-      // Load the image
-      ImageIcon imageIcon = new ImageIcon(imagePath);
-      Image image = imageIcon.getImage();
+      System.out.println("Loading image from path: " + imagePath); // Debug statement
 
-      // Scale the image to fit the JPanel
-      Image scaledImage = image.getScaledInstance(imagePanel.getWidth(), imagePanel.getHeight(), Image.SCALE_SMOOTH);
-      imageIcon = new ImageIcon(scaledImage);
+      ImageIcon imageIcon = new ImageIcon(imagePath);
+      if (imageIcon.getIconWidth() == -1) {
+        System.err.println("Error loading image from path: " + imagePath);
+        continue; // Skip this image if there's an error
+      }
+
+      // Calculate the scaling factors to fit the desired dimensions
+      double widthScale = (double) IMAGE_WIDTH / imageIcon.getIconWidth();
+      double heightScale = (double) IMAGE_HEIGHT / imageIcon.getIconHeight();
+      double scale = Math.min(widthScale, heightScale);
+
+      // Scale the image with the calculated scale factor
+      int scaledWidth = (int) (imageIcon.getIconWidth() * scale);
+      int scaledHeight = (int) (imageIcon.getIconHeight() * scale);
+      Image scaledImage = imageIcon.getImage().getScaledInstance(scaledWidth, scaledHeight, Image.SCALE_SMOOTH);
+
+      // Create a new ImageIcon with the scaled image
+      ImageIcon scaledImageIcon = new ImageIcon(scaledImage);
 
       // Add the image to the JPanel
-      JLabel imageLabel = new JLabel(imageIcon);
+      JLabel imageLabel = new JLabel(scaledImageIcon);
       imagePanel.add(imageLabel);
     }
 
